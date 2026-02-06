780c1f85a67b5258ff73bc061aac1bc3836250d1
Marcos Reis <marcos.santos@estadao.com>
2026-02-05 23:28:48 -0300

    aula 16

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { Heading } from "./components/Heading"
 export function App() {
     return (
         <>
-            <Heading />
+            <Heading>Olá dois mundos!</Heading>
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum non exercitationem enim animi, corporis porro natus repellat quisquam facere magnam. Ab ex omnis iusto. Libero ex blanditiis at eius autem.
             </p>
