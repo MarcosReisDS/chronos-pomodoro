15f5be3386bb5819fdc31af34e783116c142927c
Marcos Reis <marcos.santos@estadao.com>
2026-02-09 20:13:26 -0300

    aula 18

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import "./styles/theme.css"
 import "./styles/global.css"
 
 import { Heading } from "./components/Heading"
+import { TimerIcon } from "lucide-react"
 
 export function App() {
     return (
         <>
-            <Heading>Olá dois mundos!</Heading>
+            <Heading>
+                Olá dois mundos!
+                <button><TimerIcon /></button>
+            </Heading>
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum non exercitationem enim animi, corporis porro natus repellat quisquam facere magnam. Ab ex omnis iusto. Libero ex blanditiis at eius autem.
             </p>
