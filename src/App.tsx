a9af0671316bd48b8c9fa2f7a27656ee74a3d503
Marcos Reis <marcos.santos@estadao.com>
2026-02-23 20:36:53 -0300

    aula 41

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import "./styles/theme.css"
 import "./styles/global.css"
 
 import { Home } from "./pages/Home"
+import { AboutPomodoro } from "./pages/AboutPomodoro"
+import { NotFound } from "./pages/NotFound"
 
 export function App() {
     return <Home />
