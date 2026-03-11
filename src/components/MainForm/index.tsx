d809d5cc5049452ae0484f838b70196851cae8b6
Marcos Reis <marcos.santos@estadao.com>
2026-03-10 22:12:12 -0300

    aula 51

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -4,8 +4,12 @@ import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
 
 export function MainForm() {
+    function handleCreateNewTask(event: React.SubmitEvent<HTMLFormElement>) {
+        event.preventDefault();
+    }
+    
     return (
-        <form className="form" action="">
+        <form onSubmit={handleCreateNewTask} className="form" action="">
             <div className="formRow">
                 <DefaultInput
                     labelText="task"
