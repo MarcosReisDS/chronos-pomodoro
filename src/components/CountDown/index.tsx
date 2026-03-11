b255e2a74365cc5bbb0a9c2df357dc37a26d054e
Marcos Reis <marcos.santos@estadao.com>
2026-03-10 21:53:57 -0300

    aula 49

--- src/components/CountDown/index.tsx
+++ src/components/CountDown/index.tsx
@@ -2,11 +2,11 @@ import { useTaskContext } from "../../contexts/TaskContext"
 import styles from "./styles.module.css"
 
 export function CountDown() {
-    const taskContext = useTaskContext()
-    console.log(taskContext)
+    const { state } = useTaskContext()
+    
     return (
         <div className={styles.container}>
-           00:00
+            {state.formattedSecondsRemaining}
         </div>
     )
 }
