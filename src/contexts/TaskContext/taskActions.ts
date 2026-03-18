627dbccdaae6d15029f84545ee5d141bc7420d61
Marcos Reis <marcos.santos@estadao.com>
2026-03-17 23:03:36 -0300

    aula 68

---
+++ src/contexts/TaskContext/taskActions.ts
@@ -0,0 +1,19 @@
+import type { TaskModel } from "../../models/TaskModel"
+
+export enum TaskActionTypes {
+    START_TASK = "START_TASK",
+    INTERRUPT_TASK = "INTERRUPT_TASK",
+    RESET_STATE = "RESET_STATE"
+}
+export type TaskActionWithPayload = {
+    type: TaskActionTypes.START_TASK;
+    payload: TaskModel;
+}
+
+export type TaskActionWithoutPayload = {
+    type: TaskActionTypes.INTERRUPT_TASK
+} | {
+    type: TaskActionTypes.INTERRUPT_TASK
+}
+
+export type TaskActionModel = TaskActionWithPayload | TaskActionWithoutPayload
