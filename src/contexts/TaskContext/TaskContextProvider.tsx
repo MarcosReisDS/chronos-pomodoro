c60c42ff89e9be6ecb836e8abe69a75ed0d3ee01
Marcos Reis <marcos.santos@estadao.com>
2026-03-19 19:09:01 -0300

    aula 71

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useReducer, useState } from "react";
+import { useEffect, useReducer } from "react";
 import { initialTaskState } from "./initialTaskState";
 import { TaskContext } from "./TaskContext";
 import { taskReducer } from "./taskReducer";
