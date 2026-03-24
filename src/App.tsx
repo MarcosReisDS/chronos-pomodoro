203a04884a292b56b0fb35989e011caed3ea2b6f
Marcos Reis <marcos.santos@estadao.com>
2026-03-23 22:16:13 -0300

    aula 78

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
 import "./styles/theme.css"
 import "./styles/global.css"
 
-import { Home } from "./pages/Home"
 import { TaskContextProvider } from "./contexts/TaskContext/TaskContextProvider"
 import { MessageContainer } from "./components/MessageContainer"
-import { BrowserRouter, Route, Routes } from "react-router"
-import { NotFound } from "./pages/NotFound"
-import { AboutPomodoro } from "./pages/AboutPomodoro"
+import { MainRouter } from "./routers/MainRouter"
 
 export function App() {
     return (
         <TaskContextProvider>
             <MessageContainer>
-                <BrowserRouter>
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/about-pomodoro/" element={<AboutPomodoro />} />
-
-                        <Route path="*" element={<NotFound />} />
-                    </Routes>
-                </BrowserRouter>
+                <MainRouter />
             </MessageContainer>
         </TaskContextProvider>
     )
